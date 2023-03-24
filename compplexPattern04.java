fe24a6b29c9cd290e897789989faa56a47455847
Gauravv736 <rawatg3391@gmail.com>
2023-03-25 01:57:19 +0530

    practice_codes

--- compplexPattern04.java
+++ compplexPattern04.java
@@ -6,23 +6,17 @@ public class compplexPattern04 {
         Scanner scan = new Scanner(System.in);
         System.out.println("ENTER THE NUMBER:: ");
         int n = scan.nextInt();
-        // for (int i = 0; i < n; i++) {
-        //     System.out.println();
-        //     for (int j = 0; j < n; j++) {
-        //         if (i + j == (n - 1) / 2 || i - j == (n - 1) / 2 || j - i == (n - 1) / 2
-        //                 || i + j == ((n - 1) / 2) + n) {
-        //             System.out.print("*");
-        //         } else {
-        //             System.out.print(" ");
-        //         }
-
-        //     }
-
-        // }
+        
         System.out.println();
+        // int n = 11;
         for (int i = 0; i < n; i++) {
+            System.out.println();
             for (int j = 0; j < n; j++) {
-                if (i == 0 && j <= (n-1)/2 || j == 0 && i <= (n-1)/2 || i+j<=(n-1)/2 || i == 0 && j >=(n-1)/2 || j == n-1 && i <= (n-1)/2 || j-i >=(n-1)/2)
+                if (i==0 && j<=(n-1)/2 || j==0 && i<=(n-1)/2 || i+j<=(n-1)/2 ||
+                i==0 && j>=(n-1)/2 || j==n-1 && i<=(n-1)/2 || j-i>=(n-1)/2 ||
+                j==0 && i>(n-1)/2 || i==n-1 && j<=(n-1)/2 || i-j>=(n-1)/2 ||
+                i>=(n-1)/2 && j==n-1 || i==n-1 && j>=n/2 || i+j>=n-1+(n-1)/2
+                )
                 {
                     System.out.print("*");
                     
@@ -30,10 +24,40 @@ public class compplexPattern04 {
                 else{
                     System.out.print(" ");
                 }
+
+               
             } 
-            System.out.println();
+            System.out.print("    ");
+            for(int j=0; j<n; j++)
+        {
+            if(i+j==(n-1)/2 || i-j==(n-1)/2 || j-i==(n-1)/2 || i+j==n-1 + (n-1)/2  || i==0 || j==0 || i==n-1|| j==n-1)
+            {
+             System.out.print("*");
+            }
+            else 
+            {
+                System.out.print(" ");
+            }
+        }
+        System.out.print("   ");
+        for (int j = 0; j < n; j++) {
+            if (i + j == (n - 1) / 2 || i - j == (n - 1) / 2 || j - i == (n - 1) / 2
+                    || i + j == ((n - 1) / 2) + n) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+
+        }
+            
+            
             
         }
     }
+    // if(i==0 && j<=(n-1)/2 || j==0 && i<=(n-1)/2 || i+j<=(n-1)/2 ||
+    // i==0 && j>=(n-1)/2 || j==n-1 && i<=(n-1)/2 || j-i>=(n-1)/2 || 
+    // j==0 && i>=(n-1)/2 || i==n-1 && j<=(n-1)/2 || i-j>=(n-1)/2 ||
+    // j==n-1 && i>=(n-1)/2 || i==n-1 && j>=(n-1)/2 || i+j>=n-1 +(n-1)/2
+    // )
 
 }
---
+++ test.java
@@ -0,0 +1,24 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        int n =7;
+        for (int i = 0; i < n; i++) {
+            System.out.println();
+            for (int j = 0; j < n; j++) {
+                if (i==0&&j<=n-1 || j==0&&i<=n-1 || j==n-1&&i<=n-1 || i==n-1&&j<=n-1
+                ||i+j<=(n-1)/2 || j-i>=(n-1)/2 || i-j<=(n-1)/2 )   {
+                    System.out.print("*");
+                    
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+            
+        }
+    }
+}
